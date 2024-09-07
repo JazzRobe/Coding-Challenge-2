52dde7197c6ce42df84ce5d0e1ef8e655b4a832a
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-09-07 12:47:25 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -7,3 +7,12 @@ let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
 // Task 2: Output Details
 
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${bill + tip}.`);
+
+
+// Task 3: Create a Function calculateTip
+
+function calculateTip(bill) {
+    return tip
+};
+ 
+console.log(calculateTip(100));
